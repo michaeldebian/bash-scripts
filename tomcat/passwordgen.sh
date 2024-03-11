08a3c9400f83aabab446fdef7f92008d0700b4be
michaeldebian <michael.dellagranda@gmail.com>
2024-03-10 23:52:55 -0400

    tomcat files

--- tomcat/passwordgen.sh
+++ tomcat/passwordgen.sh
@@ -4,11 +4,10 @@
 lowercase='abcdefghijklmnopqrstuvwxyz'
 uppercase='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers='0123456789'
-#special='!@#$%^&*()-=_+[]{}|;:,.<>?'
+special='!@#$%^&*()-=_+[]{}|;:,.<>?'
 
 # Concatenate all character sets
-#all_chars="${lowercase}${uppercase}${numbers}${special}"
-all_chars="${lowercase}${uppercase}${numbers}"
+all_chars="${lowercase}${uppercase}${numbers}${special}"
 
 # Get the length of the concatenated character set
 len=${#all_chars}
@@ -27,3 +26,19 @@ done
 
 # Print the generated password
 echo "Generated password: $password"
+
+# Output the generated password to a file
+output_dir="/path/to/output/directory"
+output_file="$output_dir/generated_password.txt"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Write the password to the output file
+echo "$password" > "$output_file"
+
+# Print message about password file creation
+echo "Password saved to $output_file"
+
+# Perform sed replacement
+sed -i "s|$(cat $output_file)|<replacement_string>|g" /path/to/your/target/file
