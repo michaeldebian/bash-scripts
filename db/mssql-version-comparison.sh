527e2212e1e765f27ac7028782e85f955d90f098
michaeldebian <michael.dellagranda@gmail.com>
2024-03-11 22:13:32 -0400

    .

--- db/mssql-version-comparison.sh
+++ db/mssql-version-comparison.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 # Assuming variables.txt is the file where these variables are stored
-while [ "$RHEL_VERSION" != "correct" ] || [ "$MSSQL_VERSION" != "correct" ]; do
-    echo "Values not correct. Waiting for variables.txt to be corrected..."
+while [ "$RHEL_VERSION" -ne "$MSSQL_VERSION" ]; do
+    echo "Values do not match. Waiting for correction..."
     sleep 3
-
-    # Re-read the variables from variables.txt
-    source variables.txt
-done
 
 echo "Values corrected. Continuing with the script..."
