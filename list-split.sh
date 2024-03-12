01c726940a0ed41830718308e659898c643ca378
michaeldebian <michael.dellagranda@gmail.com>
2024-03-11 20:47:46 -0400

    list-split add

--- list-split.sh
+++ list-split.sh
@@ -9,30 +9,8 @@ if [ ! -f "$file" ]; then
     exit 1
 fi
 
-# Calculate the number of lines in the file
-num_lines=$(wc -l < "$file")
-
-# Calculate the number of lines per column
-lines_per_column=$(( (num_lines + 1) / 2 ))
-
-# Get the number of columns in the terminal
-num_columns=$(tput cols)
-
-# Calculate padding for centering the output
-padding=$(( (num_columns - 4) / 2 ))
-
 # Loop through the file and list items with numbers
 while IFS= read -r item; do
-    # Calculate padding for centering the item
-    item_padding=$(( padding - ${#item} ))
-    
-    # Print the item with padding for centering
-    printf "%*s %s" "$item_padding" "" "$line_number) $item"
-    
-    # Check if we need to move to the next column
-    if (( line_number == lines_per_column )); then
-        echo
-    fi
-    
+    echo "$line_number) $item"
     ((line_number++))
 done < "$file"
